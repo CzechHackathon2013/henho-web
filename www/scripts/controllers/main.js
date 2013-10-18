5bcf7b3a73e0a3cf1161c374894d4b04c7c02981
JSifalda <sifalda.jiri@gmail.com>
2013-10-19 01:02:13 +0200

    Init angular app

---
+++ www/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('henhoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
