3f67aec3295b42ec3f5a6e3d395ed8ef1711eac8
JSifalda <sifalda.jiri@gmail.com>
2013-10-19 20:02:17 +0200

    Fix date format

--- www/scripts/controllers/calendar.js
+++ www/scripts/controllers/calendar.js
@@ -6,7 +6,7 @@ function CalendarCtrl($scope, Api) {
   var createDateFromString = function (string) {
     var date = new Date(string);
     if(!date || date == 'Invalid Date') {
-      date = string.split('.').reverse();
+      date = string.split('/');
       date = new Date(date[0], date[1] - 1, date[2]);
     }
     return date;
